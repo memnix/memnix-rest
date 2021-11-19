65905b035eac124016ff3eab18e72e9ca27845dd
Yume <yume.network@protonmail.com>
2021-11-19 10:31:05 +0100

    gofmt

--- app/controllers/access.go
+++ app/controllers/access.go
@@ -1,8 +1,8 @@
 package controllers
 
 import (
-	"memnixrest/app/models"
 	"memnixrest/app/database"
+	"memnixrest/app/models"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
--- app/models/mem_date.go
+++ app/models/mem_date.go
@@ -9,11 +9,11 @@ import (
 // Mem structure
 type MemDate struct {
 	gorm.Model
-	UserID     uint `json:"user_id" example:"1"`
-	User       User
-	CardID     uint `json:"card_id" example:"1"`
-	Card       Card
-	DeckID     uint `json:"deck_id" example:"1"`
-	Deck       Deck
-	NextDate   time.Time `json:"next_date" example:"06/01/2003"`// gorm:"autoCreateTime"`
+	UserID   uint `json:"user_id" example:"1"`
+	User     User
+	CardID   uint `json:"card_id" example:"1"`
+	Card     Card
+	DeckID   uint `json:"deck_id" example:"1"`
+	Deck     Deck
+	NextDate time.Time `json:"next_date" example:"06/01/2003"` // gorm:"autoCreateTime"`
 }
