872de8911e7a91efc17c9468315b5571df9f699a
Yume <yume.network@protonmail.com>
2021-11-20 18:59:53 +0100

    Remove unused routes

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -50,11 +50,13 @@ func New() *fiber.App {
 	registerDeckRoutes(v1) // /v1/decks/
 	registerCardRoutes(v1) // /v1/cards/
 
+	/* DEPRECATED ROUTES
 	// Mem
 	v1.Get("/mems/id/:id", controllers.GetMemByID)                             // Get mem by ID
 	v1.Get("/mems/user/:userID/card/:cardID", controllers.GetMemByCardAndUser) // Get mem by userID & cardID
 	v1.Post("/mems/new", controllers.CreateNewMem)                             // Create a new mem
 	v1.Put("/mem/id/:id", controllers.UpdateMemByID)                           // Update mem by ID
+
 
 	// Access
 	v1.Get("/accesses", controllers.GetAllAccesses)                                       // Get all accesses
@@ -64,11 +66,13 @@ func New() *fiber.App {
 	v1.Post("/accesses/new", controllers.CreateNewAccess)                                 // Create a new access
 	v1.Put("/accesses/id/:id", controllers.UpdateAccessByID)                              // Update an access using his ID
 
+
 	// Answer
 	v1.Get("/answers", controllers.GetAllAnswers)                   // Get all answers
 	v1.Get("/answers/id/:id", controllers.GetAnswerByID)            // Get answer by ID
 	v1.Get("/answers/card/:cardID", controllers.GetAnswersByCardID) // Get answer by CardID
 	v1.Post("/answers/new", controllers.CreateNewAnswer)            // Create a new answer
+	*/
 
 	return app
 }
