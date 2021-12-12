1e4786440d4810386e25bac448c6063acc71a459
Yume <yume.network@protonmail.com>
2021-12-12 11:15:57 +0100

    Rating routes

--- pkg/routes/ratingRoutes.go
+++ pkg/routes/ratingRoutes.go
@@ -9,11 +9,11 @@ import (
 func registerRatingRoutes(r fiber.Router) {
 
 	// Get
-	r.Get("/ratings", controllers.GetAllRatings) // Get all ratings
-	r.Get("/ratings/deck/:deckID", controllers.GetAllRatingsByDeck)
-	r.Get("/ratings/deck/:deckID/average", controllers.GetAverageRatingByDeck)
-	r.Get("/ratings/deck/:deckID/user/:userID", controllers.GetRatingByDeckAndUser)
-	r.Get("/ratings/deck/:deckID/user", controllers.GetRatingByDeckAndUser)
+	r.Get("/ratings", controllers.GetAllRatings)                                    // Get all ratings
+	r.Get("/ratings/deck/:deckID", controllers.GetAllRatingsByDeck)                 // Get all ratings by deck
+	r.Get("/ratings/deck/:deckID/average", controllers.GetAverageRatingByDeck)      // Get average rating by deck
+	r.Get("/ratings/deck/:deckID/user/:userID", controllers.GetRatingByDeckAndUser) // Get rating by deck and user
+	r.Get("/ratings/deck/:deckID/user", controllers.GetRatingsByDeck)               // Get rating by deck
 
 	// Post
 	r.Post("/ratings/new", controllers.RateDeck) // Rate a deck
