9a6e54c29489b13ce07ca75c1c3cca23c3c5ae46
Yume <yume.network@protonmail.com>
2021-12-19 10:53:07 +0100

    MEM-151 Logs table

---
+++ app/models/logs.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+// Mem structure
+type Logs struct {
+	gorm.Model
+	LogType LogType `json:"log_type" example:"1"`
+	Message string  `json:"log_message" example:"Edited Profile Picture"`
+}
+
+type LogType int64
+
+const (
+	LogUndefined LogType = iota
+	LogUserLogin
+	LogUserLogout
+	LogUserRegister
+	LogUserEdit
+	LogUserDeleted
+	LogSubscribe
+	LogUnsubscribe
+	LogDeckCreated
+	LogDeckDeleted
+	LogDeckEdited
+)
