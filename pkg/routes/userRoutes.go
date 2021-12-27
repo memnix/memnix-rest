721378b05bb9f2fdabfa93939197598089135c3b
Yume <yume.network@protonmail.com>
2021-12-27 16:36:24 +0100

    remove migrate

--- pkg/routes/userRoutes.go
+++ pkg/routes/userRoutes.go
@@ -11,8 +11,6 @@ func registerUserRoutes(r fiber.Router) {
 	r.Get("/users", controllers.GetAllUsers)        // Get all users
 	r.Get("/users/id/:id", controllers.GetUserByID) // Get user by ID
 
-	r.Get("/secret/migrate", controllers.SecretMigrate)
-
 	// Put
 	r.Put("/users/id/:id", controllers.UpdateUserByID) // Update an user using his ID
 
