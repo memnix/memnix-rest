f36924ea87738e9d5f2b94d0a88e72fcbfc57f00
Yume <yume.network@protonmail.com>
2022-02-18 15:12:43 +0100

    Simplify UpdateMem()

--- pkg/core/memnix.go
+++ pkg/core/memnix.go
@@ -8,6 +8,71 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// ComputeInterval function
+func ComputeInterval(r *models.Mem) uint {
+	switch r.Repetition {
+	case 0:
+		return 1
+	case 1, 2:
+		return 2
+	case 3:
+		return 3
+	default:
+		return uint(float32(r.Interval)*r.Efactor*0.75) + 1
+	}
+}
+
+func ComputeQualitySuccess(memType int, repetition uint) uint {
+
+	if memType == 2 {
+		return 3
+	} else {
+		if repetition > 3 {
+			return 5
+		}
+		return 4
+	}
+}
+
+func ComputeQualityFailed(memType int, repetition uint) uint {
+	if memType == 2 {
+		if repetition <= 3 {
+			return 0
+		}
+		return 1
+	}
+	if repetition <= 4 {
+		return 1
+	}
+	return 2
+
+}
+
+func ComputeEfactor(r *models.Mem) float32 {
+	eFactor := r.Efactor + (0.1 - (5.0-float32(r.Quality))*(0.08+(5-float32(r.Quality)))*0.02)
+
+	if eFactor < 1.3 {
+		return 1.3
+	}
+	return eFactor
+}
+
+func UpdateMemDate(mem *models.Mem) {
+	db := database.DBConn
+
+	memDate := new(models.MemDate)
+
+	_ = db.Joins("Card").Joins("User").Joins("Deck").Where("mem_dates.user_id = ? AND mem_dates.card_id = ?",
+		mem.UserID, mem.CardID).First(&memDate).Error
+
+	memDate.NextDate = time.Now().AddDate(0, 0, int(mem.Interval))
+
+	db.Save(memDate)
+
+	//TODO: Return error
+
+}
+
 // UpdateMem function
 func UpdateMem(_ *fiber.Ctx, r *models.Mem, validation models.CardResponseValidation) {
 	//TODO: Rewrite functions
@@ -16,8 +81,7 @@ func UpdateMem(_ *fiber.Ctx, r *models.Mem, validation models.CardResponseValida
 
 	mem := new(models.Mem)
 
-	mem.UserID = r.UserID
-	mem.CardID = r.CardID
+	mem.UserID, mem.CardID = r.UserID, r.CardID
 
 	var memType int
 
@@ -26,56 +90,21 @@ func UpdateMem(_ *fiber.Ctx, r *models.Mem, validation models.CardResponseValida
 	}
 
 	if validation.Validate {
-		if r.Repetition == 0 {
-			mem.Interval = 1
-		} else if r.Repetition == 1 {
-			mem.Interval = 2
-		} else if r.Repetition == 2 {
-			mem.Interval = 2
-		} else if r.Repetition == 3 {
-			mem.Interval = 3
-		} else {
-			mem.Interval = uint(float32(r.Interval)*r.Efactor*0.75) + 1
-		}
+		mem.Interval = ComputeInterval(r)
 		mem.Repetition = r.Repetition + 1
-		if memType == 2 {
-			r.Quality = 3
-		} else {
-			r.Quality = 4
-			if r.Repetition > 3 {
-				r.Quality = 5
-			}
-		}
+		r.Quality = ComputeQualitySuccess(memType, r.Repetition)
+
 	} else {
 		mem.Repetition = 0
 		mem.Interval = 0
-		if memType == 2 {
-			r.Quality = 1
-			if r.Repetition <= 1 {
-				r.Quality = 0
-			}
-		} else {
-			r.Quality = 2
-		}
+		r.Quality = ComputeQualityFailed(memType, r.Repetition)
 	}
-	mem.Efactor = r.Efactor + (0.1 - (5.0-float32(r.Quality))*(0.08+(5-float32(r.Quality)))*0.02)
 
-	if mem.Efactor < 1.3 {
-		mem.Efactor = 1.3
-	}
+	mem.Efactor = ComputeEfactor(r)
 
 	db.Save(r)
 	db.Create(mem)
 
-	memDate := new(models.MemDate)
+	UpdateMemDate(mem)
 
-	if err := db.Joins("Card").Joins("User").Joins("Deck").Where("mem_dates.user_id = ? AND mem_dates.card_id = ?",
-		mem.UserID, mem.CardID).First(&memDate).Error; err != nil {
-	}
-
-	memDate.NextDate = time.Now().AddDate(0, 0, int(mem.Interval))
-
-	db.Save(memDate)
-
-	//TODO: Return error
 }
