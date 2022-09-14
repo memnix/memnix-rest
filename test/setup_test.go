e0a05cbad1660899b03ebb79620d9bbb0e849a23
CorentinGS <c.giaufersaubert@outlook.com>
2022-09-14 22:15:51 +0200

    🐛 Fix test

--- test/setup_test.go
+++ test/setup_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/memnix/memnixrest/app/routes"
 	"github.com/memnix/memnixrest/pkg/database"
 	"github.com/memnix/memnixrest/pkg/models"
+	"github.com/memnix/memnixrest/pkg/queries"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 	"testing"
@@ -67,6 +68,8 @@ func Setup() (*fiber.App, error) {
 
 	// Create the app
 	app := routes.New()
+
+	queries.InitCache()
 
 	return app, nil
 }
