c443ffa12d3aa140351ad6ec3a6ab10e16320c9f
Restyled.io <commits@restyled.io>
2023-03-12 14:50:34 +0000

    Restyled by shfmt

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,10 +1,9 @@
 export GOPATH="$(go env GOPATH)"
 export PATH="$PATH:$GOPATH/bin"
 
-echo "Starting go fumpt" && gofumpt -l -w . && \
-echo "Starting fieldalignment" && fieldalignment -fix domain/**  && \
-echo "Starting gci" && gci write -s standard -s default  . && \
-echo "Starting golangci-lint" &&   golangci-lint run && \
-echo "Starting go test" && \
-go test -cover  ./...
-
+echo "Starting go fumpt" && gofumpt -l -w . &&
+  echo "Starting fieldalignment" && fieldalignment -fix domain/** &&
+  echo "Starting gci" && gci write -s standard -s default . &&
+  echo "Starting golangci-lint" && golangci-lint run &&
+  echo "Starting go test" &&
+  go test -cover ./...
