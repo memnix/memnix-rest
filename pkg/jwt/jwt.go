7ebea10247906e5e5be4562b5937aba6de576aa7
CorentinGS <contact@corentings.dev>
2024-02-22 20:12:46 +0100

    ✅ test(pkg): add tests

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -37,10 +37,10 @@ func (j *InstanceSingleton) SetJwt(instance Instance) {
 }
 
 type Instance struct {
-	headerLen             int
-	publicKey             ed25519.PublicKey
-	privateKey            ed25519.PrivateKey
-	signingMethod         jwt.SigningMethod
+	HeaderLen             int
+	PublicKey             ed25519.PublicKey
+	PrivateKey            ed25519.PrivateKey
+	SigningMethod         jwt.SigningMethod
 	ExpirationTimeInHours int
 }
 
@@ -49,10 +49,10 @@ func NewJWTInstance(headerLen, expirationTime int,
 	publicKey ed25519.PublicKey, privateKey ed25519.PrivateKey,
 ) Instance {
 	return Instance{
-		headerLen:             headerLen,
-		publicKey:             publicKey,
-		privateKey:            privateKey,
-		signingMethod:         jwt.SigningMethodEdDSA,
+		HeaderLen:             headerLen,
+		PublicKey:             publicKey,
+		PrivateKey:            privateKey,
+		SigningMethod:         jwt.SigningMethodEdDSA,
 		ExpirationTimeInHours: expirationTime,
 	}
 }
@@ -66,13 +66,13 @@ func NewJWTInstance(headerLen, expirationTime int,
 // see: utils/config.go for more information
 func (instance Instance) GenerateToken(_ context.Context, userID uint) (string, error) {
 	// Create the Claims for the token
-	claims := jwt.NewWithClaims(instance.signingMethod, jwt.RegisteredClaims{
+	claims := jwt.NewWithClaims(instance.SigningMethod, jwt.RegisteredClaims{
 		Issuer:    utils.ConvertUIntToStr(userID),     // Issuer is the user id
 		ExpiresAt: instance.CalculateExpirationTime(), // ExpiresAt is the expiration time
 	})
 
 	// Sign and get the complete encoded token as a string using the secret
-	token, err := claims.SignedString(instance.privateKey)
+	token, err := claims.SignedString(instance.PrivateKey)
 	if err != nil {
 		return "", errors.Wrap(err, "failed to sign")
 	}
@@ -105,7 +105,7 @@ func (instance Instance) GetToken(_ context.Context, token string) (*jwt.Token, 
 		if _, ok := token.Method.(*jwt.SigningMethodEd25519); !ok {
 			return nil, errors.New("unexpected signing method")
 		}
-		return instance.publicKey, nil // Return the secret key as the signing key
+		return instance.PublicKey, nil // Return the secret key as the signing key
 	})
 }
 
@@ -118,11 +118,11 @@ func (Instance) GetExpirationTime(token *jwt.Token) int64 {
 	return int64(claims["exp"].(float64))
 }
 
-// extractToken function to extract token from header.
-func (instance Instance) extractToken(token string) string {
+// ExtractToken function to extract token from header.
+func (instance Instance) ExtractToken(token string) string {
 	// Normally Authorization HTTP header.
 	onlyToken := strings.Split(token, " ") // Split token.
-	if len(onlyToken) == instance.headerLen {
+	if len(onlyToken) == instance.HeaderLen {
 		return onlyToken[1] // Return only token.
 	}
 	return "" // Return empty string.
@@ -130,18 +130,30 @@ func (instance Instance) extractToken(token string) string {
 
 // GetConnectedUserID gets the user id from a jwt token.
 func (instance Instance) GetConnectedUserID(ctx context.Context, tokenHeader string) (uint, error) {
+	if tokenHeader == "" {
+		return 0, errors.New("empty token")
+	}
+
 	// Get the token from the Authorization header.
-	tokenString := instance.extractToken(tokenHeader)
+	tokenString := instance.ExtractToken(tokenHeader)
+
+	if tokenString == "" {
+		return 0, errors.New("empty token")
+	}
 
 	token, err := instance.GetToken(ctx, tokenString)
 	if err != nil {
-		return 0, err
+		return 0, errors.New("invalid token")
 	}
 
 	// Check if the token is valid.
 	userID, err := instance.VerifyToken(token)
 	if err != nil {
 		return 0, err
+	}
+
+	if userID == 0 {
+		return 0, errors.New("invalid token")
 	}
 
 	return userID, nil
---
+++ pkg/random/random_test.go
@@ -0,0 +1,108 @@
+package random_test
+
+import (
+	"errors"
+	"strings"
+	"testing"
+
+	"github.com/memnix/memnix-rest/pkg/random"
+)
+
+func TestGenerator_GenerateSecretCode(t *testing.T) {
+	generator := random.GetRandomGeneratorInstance()
+
+	testCases := []struct {
+		name        string
+		n           int
+		expected    int
+		expectedErr error
+		hasErr      bool
+	}{
+		{
+			name:        "GenerateSecretCodeWithLength10",
+			n:           10,
+			expected:    10,
+			expectedErr: nil,
+			hasErr:      false,
+		},
+		{
+			name:        "GenerateSecretCodeWithLength20",
+			n:           20,
+			expected:    20,
+			expectedErr: nil,
+			hasErr:      false,
+		},
+		{
+			name:        "GenerateSecretCodeWithLength0",
+			n:           0,
+			expected:    0,
+			expectedErr: nil,
+			hasErr:      false,
+		},
+		{
+			name:        "GenerateSecretCodeWithLengthNegative",
+			n:           -1,
+			expected:    0,
+			expectedErr: errors.New("invalid length"),
+			hasErr:      true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			code, err := generator.GenerateSecretCode(tc.n)
+			if tc.hasErr && err == nil {
+				t.Fatalf("Expected error, but got nil")
+			}
+
+			if !tc.hasErr && err != nil {
+				t.Fatalf("Unexpected error: %v", err)
+			}
+
+			if tc.hasErr && err != nil && err.Error() != tc.expectedErr.Error() {
+				t.Fatalf("Expected error to be %v, but got %v", tc.expectedErr, err)
+			}
+
+			if len(code) != tc.expected {
+				t.Errorf("Expected code length to be %d, but got %d", tc.expected, len(code))
+			}
+
+			if !isValidCode(code) {
+				t.Errorf("Generated code is not valid: %s", code)
+			}
+		})
+	}
+}
+
+func isValidCode(code string) bool {
+	// Define the valid characters for the secret code
+	validChars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	// Check if each character in the code is a valid character
+	for _, c := range code {
+		if !strings.ContainsRune(validChars, c) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func FuzzGenerateSecretCode(f *testing.F) {
+	generator := random.GetRandomGeneratorInstance()
+	f.Add(uint(1000))
+	f.Fuzz(func(t *testing.T, size uint) {
+		code, err := generator.GenerateSecretCode(int(size))
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+
+		if len(code) != int(size) {
+			t.Errorf("Expected code length to be %d, but got %d", size, len(code))
+		}
+
+		if !isValidCode(code) {
+			t.Errorf("Generated code is not valid: %s", code)
+		}
+	})
+}
